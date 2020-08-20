b6e66b5e78c36564c4b0a59cf0a46319c1210fdb
Yixin Luo <18810541851@163.com>
2020-08-20 22:57:59 +0800

    change name

--- Core/CMakeFiles/Makefile.cmake
+++ Core/CMakeFiles/Makefile.cmake
@@ -46,5 +46,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test_luolkv.dir/DependInfo.cmake"
+  "CMakeFiles/test_legokv.dir/DependInfo.cmake"
   )
