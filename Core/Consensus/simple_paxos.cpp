fb7950f9352626f046e6e91ba7d4ff8fc708cfc1
luohaha <18810541851@163.com>
2020-08-30 11:59:14 +0800

    finish paxos state machine

--- Core/Consensus/simple_paxos.cpp
+++ Core/Consensus/simple_paxos.cpp
@@ -1,4 +1,5 @@
 #include "simple_paxos.h"
+#include <algorithm>
 
 namespace lkv 
 {
@@ -9,6 +10,7 @@ int SimplePaxos::Propose(const std::string &consensus_group,
                       const ConsensusType &value,
                       std::function<int (bool, const std::string &, const ConsensusType &)> cb)
 {
+    
     return cb(true, consensus_group, value);
 }
 
@@ -20,6 +22,101 @@ std::string &SimplePaxos::ChooseReadProvider(const std::string &consensus_group)
 std::string &SimplePaxos::ChooseModifyProvider(const std::string &consensus_group)
 {
     return conf_->GetConf().gconf.globalConsensusGroup[consensus_group].at(0);
+}
+
+Status SimplePaxos::HandleAccept(ServerContext* context, 
+                                const simplepaxos::Accept* request, 
+                                simplepaxos::AcceptRet* response)
+{
+    if (request->acceptern() >= acceptor_.proposaln) {
+        acceptor_.proposaln = request->acceptern();
+        acceptor_.acceptvalue = request->acceptern();
+        acceptor_.acceptvalue = request->accepterv();
+        response->set_proposaln(request->acceptern());
+        response->set_isok(true);
+        return Status::OK;
+    } else {
+        response->set_isok(false);
+        return Status::OK;
+    }
+}
+Status SimplePaxos::HandlePrepare(ServerContext* context, 
+                                  const simplepaxos::Prepare* request, 
+                                  simplepaxos::PrepareRet* response)
+{
+    if (request->proposaln() > acceptor_.proposaln) {
+        acceptor_.proposaln = request->proposaln();
+        response->set_proposaln(acceptor_.proposaln);
+        response->set_acceptern(acceptor_.acceptn);
+        response->set_accepterv(acceptor_.acceptvalue);
+        response->set_isok(true);
+        return Status::OK;
+    } else {
+        response->set_isok(false);
+        return Status::OK;
+    }
+}
+
+Status SimplePaxos::SendPrepare_(std::string peer, int64_t proposaln)
+{
+    if (stub_pool_.find(peer) == stub_pool_.end()) {
+        stub_pool_[peer](simplepaxos::SimplePaxos::NewStub(grpc::CreateChannel(peer, 
+          grpc::InsecureChannelCredentials())));
+    }
+    ClientContext context;
+    simplepaxos::Prepare p;
+    p.set_proposaln(proposaln);
+    simplepaxos::PrepareRet ret;
+    Status s = stub_pool_[peer]->HandlePrepare(&context, p, &ret);
+    if (s.ok() && ret.isok()) {
+        proposer_.pset.insert(peer);
+        proposer_.proposaln = std::max(ret.proposaln(), proposer_.proposaln);
+        if (ret.acceptern() > proposer_.maxacceptn) {
+            proposer_.maxacceptv = ret.accepterv();
+        }
+        if (proposer_.pset.size() > peerset_.size() / 2) {
+            proposer_.state = ProposerState::ACCEPT;
+        }
+    }
+    return s;
+}
+    
+Status SimplePaxos::SendAccept_(std::string peer, int64_t acceptn, std::string acceptv)
+{
+    if (stub_pool_.find(peer) == stub_pool_.end()) {
+        stub_pool_[peer](simplepaxos::SimplePaxos::NewStub(grpc::CreateChannel(peer, 
+          grpc::InsecureChannelCredentials())));
+    }
+    ClientContext context;
+    simplepaxos::Accept a;
+    a.set_acceptern(acceptn);
+    a.set_accepterv(acceptv);
+    simplepaxos::AcceptRet ret;
+    Status s = stub_pool_[peer]->HandleAccept(&context, a, &ret);
+    if (s.ok() && ret.isok()) {
+        proposer_.aset.insert(peer);
+        if (proposer_.aset.size() > peerset_.size() / 2) {
+            proposer_.state = ProposerState::CONFIRM;
+        }
+    }
+    return s;
+}
+
+void SimplePaxos::Drive_()
+{
+    do {
+        if (proposer_.state == ProposerState::PREPARE) {
+            for (auto &peer : peerset_) {
+                SendPrepare_(peer, 0);
+            }
+        } else if (proposer_.state == ProposerState::ACCEPT) {
+            for (auto &peer : peerset_) {
+                SendAccept_(peer, proposer_.maxacceptn, proposer_.maxacceptv);
+            }
+        } else if (proposer_.state == ProposerState::CONFIRM) {
+
+        } else {}
+    } while (true);
 }
 
 }
--- Core/Consensus/simple_paxos.h
+++ Core/Consensus/simple_paxos.h
@@ -1,14 +1,55 @@
 #ifndef SIMPLE_PAXOS_H
 #define SIMPLE_PAXOS_H
 
+#include "simple_paxos.pb.h"
+#include "simple_paxos.grpc.pb.h"
 #include "consensus.h"
 #include "conf.h"
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/grpcpp.h>
+
+using namespace grpc;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
 
 namespace lkv 
 {
+using namespace Rpc;
 namespace Consensus
 {
-class SimplePaxos : public IConsensus
+struct Acceptor
+{
+int64_t proposaln = 0;
+int64_t acceptn = 0;
+std::string acceptvalue;
+bool is_confirmed = false;
+};
+
+enum ProposerState
+{
+    INIT,
+    PREPARE,
+    ACCEPT,
+    CONFIRM
+};
+
+struct Proposer
+{
+std::set<std::string> pset;
+std::set<std::string> aset;
+int64_t proposaln = 0;
+int64_t maxacceptn = 0;
+std::string maxacceptv;
+std::string proposev;
+ProposerState state = ProposerState::INIT;
+};
+
+class SimplePaxos : public IConsensus, public simplepaxos::SimplePaxos::Service
 {
 public:
     SimplePaxos(Conf::IConf *conf) : conf_(conf) 
@@ -19,8 +60,22 @@ public:
                         std::function<int (bool, const std::string&, const ConsensusType &)> cb) override;
     virtual std::string &ChooseReadProvider(const std::string &consensus_group) override;
     virtual std::string &ChooseModifyProvider(const std::string &consensus_group) override;
+
+    // rpc impl
+    virtual Status HandleAccept(ServerContext* context, 
+    const simplepaxos::Accept* request, simplepaxos::AcceptRet* response);
+    virtual Status HandlePrepare(ServerContext* context, 
+    const simplepaxos::Prepare* request, simplepaxos::PrepareRet* response);
+private:
+    Status SendPrepare_(std::string peer, int64_t proposaln);
+    Status SendAccept_(std::string peer, int64_t acceptn, std::string acceptv);
+    void Drive_();
 private:
     Conf::IConf *conf_;
+    std::map<std::string, std::unique_ptr<simplepaxos::SimplePaxos::Stub>> stub_pool_;
+    Acceptor acceptor_;
+    Proposer proposer_;
+    std::vector<std::string> peerset_;
 };
 
 }
