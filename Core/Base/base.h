56835ab24ac23a22dddf5320368eaaf91d196f88
luohaha <18810541851@163.com>
2020-09-05 23:12:04 +0800

    add PageContainer impl

--- Core/Base/base.h
+++ Core/Base/base.h
@@ -12,8 +12,17 @@
 #include <cassert>
 #include "leveldb/db.h"
 #include "errcode.h"
+#include <memory>
 #include <vector>
 #include <map>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 namespace lkv 
 {
@@ -64,6 +73,97 @@ namespace Base
 
   private:
     LogLevel level_;
+  };
+
+  class File
+  {
+  public:
+    File(const std::string &file_name, const uint64_t size) 
+    {
+      ok_ = true;
+      size_ = size;
+      fd_ = open(file_name.c_str(), O_CREAT | O_RDWR);
+      if (fd_ < 0) {
+        ok_ = false;
+      }
+      if (ftruncate(fd_, size) < 0) {
+        ok_ = false;
+      }
+    }
+    ~File() { if (fd_ >= 0) { close(fd_); } }
+    bool Ok() { return ok_; }
+    void PWrite(const void *buf, size_t count, int offset)
+    {
+      if (offset + count > size_) {
+        ok_ = false;
+      } else {
+        if (pwrite(fd_, buf, count, offset) < 0) {
+          ok_ = false;
+        }
+      }
+    }
+    void PRead(void *buf, size_t count, int offset)
+    {
+      if (offset + count > size_) {
+        ok_ = false;
+      } else {
+        if (pread(fd_, buf, count, offset) < 0) {
+          ok_ = false;
+        }
+      }
+    }
+  private:
+    int fd_;
+    uint64_t size_;
+    bool ok_;
+  };
+  
+  class PageContainer
+  {
+  public:
+    PageContainer(const std::string &dir)
+    {
+      dir_ = dir;
+      int ret = mkdir(dir_.c_str(), 0777);
+      if (ret == 0 || errno == EEXIST) {
+        ok_ = true;
+      } else {
+        ok_ = false;
+      }
+    }
+    ~PageContainer() 
+    {
+      for (auto &each :file_index_) {
+        delete each->second;
+      }
+    }
+    static const uint64_t PAGE_SIZE = 4096;
+    static const uint64_t FILE_SIZE = PAGE_SIZE * 1024 * 1024;
+    static const uint64_t PAGE_PER_FILE = 1024 * 1024;
+    inline uint64_t GET_FILE(uint64_t index) const { return index / PAGE_PER_FILE; }
+    inline uint64_t GET_PAGE(uint64_t index) const { return index % PAGE_PER_FILE; }
+    inline File *AllocFile(uint64_t index) 
+    { return new File(dir_ + "/" + std::to_string(GET_FILE(index)), FILE_SIZE); }
+    bool Ok() const { return ok_; }
+    void writepage(uint64_t index, const char *buf, int size) 
+    {
+      if (file_index_.find(GET_FILE(index)) == file_index_.end()) {
+        file_index_.emplace(GET_FILE(index), AllocFile(index));
+      }
+      file_index_[index]->PWrite(buf, size, GET_PAGE(index) * PAGE_SIZE);
+      ok_ = file_index_[index]->Ok();
+    }
+    void readpage(uint64_t index, char *buf, int size)
+    {
+      if (file_index_.find(GET_FILE(index)) != file_index_.end()) {
+        file_index_[index]->PRead(buf, size, GET_PAGE(index) * PAGE_SIZE);
+        ok_ = file_index_[index]->Ok();
+      }
+    }
+  private:
+    std::string dir_;
+    std::map<uint64_t, File*> file_index_;
+    bool ok_;
   };
 } // namespace Base
 }
--- Core/Base/errcode.h
+++ Core/Base/errcode.h
@@ -7,9 +7,11 @@
 #define ERR_UNEXPECTED 1
 #define ERR_NOT_FOUND 2
 #define ERR_JSON_PARSER_ERR 3
+#define ERR_CONSENSUS_NO_EXIST 4
 
 // err info map
- static const char *err_info_map[4]
- = {"ERR_SUCCESS", "ERR_UNEXPECTED", "ERR_NOT_FOUND", "ERR_JSON_ERR"};
+ static const char *err_info_map[5]
+ = {"ERR_SUCCESS", "ERR_UNEXPECTED", "ERR_NOT_FOUND", "ERR_JSON_PARSER_ERR",
+    "ERR_CONSENSUS_NO_EXIST"};
 
 #endif
